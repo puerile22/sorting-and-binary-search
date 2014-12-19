1bfefec04c9e01f6d234480cefabc04d05624cfd
Peng Gao <puerile22@gmail.com>
2014-12-19 09:24:36 -0800

    fix space issue

--- bsearch.rb
+++ bsearch.rb
@@ -5,9 +5,8 @@ def bsearch(arr,num,i=0)
   return i+1 if arr.length == 1 && num>arr[0]
   return i if arr.length == 1 && num<arr[0]
   if num > arr[arr.length/2]
-    i+=arr.length/2
-    arr=arr[arr.length/2...arr.length]
-    #binding.pry
+    i += arr.length/2
+    arr = arr[arr.length/2...arr.length]
     puts i
   else
     arr = arr[0...arr.length/2]
@@ -18,17 +17,17 @@ end
 def ibsearch(arr,num)
   i = arr.length
   j = 0
-  while(i!=1)
-    return j+arr.length/2 if num == arr[arr.length/2]
+  while(i != 1)
+    return j + arr.length/2 if num == arr[arr.length/2]
     if num > arr[arr.length/2]
-      j+=arr.length/2
-      arr=arr[arr.length/2...arr.length]
+      j += arr.length/2
+      arr = arr[arr.length/2...arr.length]
     else
       arr = arr[0...arr.length/2]
     end
     i = arr.length
   end
-  return j+arr.length/2 if num == arr[arr.length/2]
-  return j+1 if num>arr[0]
-  return j if num<arr[0]
+  return j + arr.length/2 if num == arr[arr.length/2]
+  return j + 1 if num > arr[0]
+  return j if num < arr[0]
 end
--- lib/sort.rb
+++ lib/sort.rb
@@ -1,40 +1,43 @@
 class Sorting
   def sort(arr) 
     (1...arr.length).each do |i|
-      (i+1..arr.length).each do |j|
-        arr[-i],arr[-j] = arr[-j],arr[-i] if arr[-i]<arr[-j]
+      (i + 1..arr.length).each do |j|
+        arr[-i],arr[-j] = arr[-j],arr[-i] if arr[-i] < arr[-j]
       end
     end
     return arr
   end
 
   def mergesort(arr)
-    arr2=[]
-    split(arr,0,arr.length-1,arr2)
+    arr2 = []
+    split(arr,0,arr.length - 1,arr2)
     return arr2
   end
+
   def copy(arr1,i,j,arr2)
     (i..j).each do |k|
       arr2[k] = arr1[k]
     end
   end
+
   def split(arr1,i,j,arr2)
-    return if j-i<1
-    k = (j+i)/2
+    return if j - i < 1
+    k = (j + i)/2
     split(arr1,i,k,arr2)
-    split(arr1,k+1,j,arr2)
+    split(arr1,k + 1,j,arr2)
     merge(arr1,i,j,k,arr2)
     copy(arr2,i,j,arr1)
   end
+  
   def merge(arr1,i,j,k,arr2)
-    a,b = i,k+1
+    a,b = i,k + 1
     (i..j).each do |c|
-      if a<k+1 && (b>j || arr1[a]<arr1[b])
+      if a < k + 1 && (b > j || arr1[a] < arr1[b])
         arr2[c] = arr1[a]
-        a+=1
+        a += 1
       else
         arr2[c] = arr1[b]
-        b+=1
+        b += 1
       end
     end
   end
